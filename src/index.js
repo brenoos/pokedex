30132a4a316d7e82bcbb82102aeaf087602aeca4
Breno Oliveira dos Santos <breno.oliver07@gmail.com>
2018-08-05 19:51:48 -0300

    removendo redux

--- src/index.js
+++ src/index.js
@@ -5,24 +5,12 @@ import './template/custom.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-import { applyMiddleware, createStore } from 'redux'
-import { Provider } from 'react-redux'
 import {HashRouter as Router} from 'react-router-dom'
 
-import thunk from 'redux-thunk'
-import reducers from './reducers/rootReducer'
-
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
-    && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = applyMiddleware(thunk)(createStore)(reducers, devTools)
-
-
 ReactDOM.render(
-    <Provider store={store} >
         <Router>
             <App />
         </Router>
-    </Provider>
     , document.getElementById('root')
 )
 registerServiceWorker()
--- src/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import pkmReducer from './pkmReducer'
-
-const rootReducer = combineReducers({
-    pkm: pkmReducer
-})
-
-export default rootReducer
